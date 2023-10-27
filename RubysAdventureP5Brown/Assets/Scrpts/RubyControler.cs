ff6476907d43096e6929480849afd1265a7e3c43
Kayden Brown <145067189+Buried7564@users.noreply.github.com>
2023-10-27 13:51:31 -0700

    finished movement started world design.

--- RubysAdventureP5Brown/Assets/Scrpts/RubyControler.cs
+++ RubysAdventureP5Brown/Assets/Scrpts/RubyControler.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
 
 public class RubyControler : MonoBehaviour
@@ -7,14 +8,20 @@ public class RubyControler : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = 10;
         
     }
 
     // Update is called once per frame
     void Update()
     {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
         Vector2 position = transform.position;
-        position.x = position.x + 0.01f;
+        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
+        position.y = position.y + 3.0f * vertical * Time.deltaTime;
         transform.position = position;
     }
 }
